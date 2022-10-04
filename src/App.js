ad488f4552ccc4b866318474759d097c403c7a4c
Istiak Ahmed <istiakahmed024@gmail.com>
2022-10-05 00:25:50 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>New repo</h1>
+      <h2>Fixing Problems</h2>
+    </div>
+  );
+}
+
+export default App;
